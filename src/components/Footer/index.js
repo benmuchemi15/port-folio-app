656765ff231245de7905c40eb9a59a72a6ea3fb8
Benson Muchemi <Ben@Bensons-MacBook-Pro.local>
2021-09-14 23:02:10 -0400

    footer code

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer () {
+    return(
+        <div className="footer">
+            <a href="https://github.com/benmuchemi15"><img src="https://img.icons8.com/ios/80/000000/github.png" alt="github-logo" class="icon"/></a>
+            <a href="https://www.linkedin.com/in/benson-muchemi-b689b9211/"><img src="https://img.icons8.com/fluent/80/000000/linkedin-circled.png" alt="linkedin-logo" class="icon"/></a>
+        </div>
+    )
+}
+
+export default Footer;
