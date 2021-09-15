1786a7f2d563389aa37300d7a8af0ac12838fc00
Benson Muchemi <Ben@Bensons-MacBook-Pro.local>
2021-09-14 23:01:43 -0400

    contact page

---
+++ src/components/Contact/index.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import { validateEmail } from '../../utils/helper'
+
+function contactInfo() {
+    return(
+        <section>
+            <h1 className="contact">
+            Contact 
+            </h1>
+        </section>
+    )
+}
+
+export default contactInfo;
